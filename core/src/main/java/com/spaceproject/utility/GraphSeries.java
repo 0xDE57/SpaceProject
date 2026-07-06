965f7e9e8ba0c602dd9b66a0fa1daff90aeb925c
arbitrary hexcode <0XDE57@users.noreply.github.com>
2026-07-06 05:38:21 -0600

    fix white space with Reformat Code

--- core/src/main/java/com/spaceproject/utility/GraphSeries.java
+++ core/src/main/java/com/spaceproject/utility/GraphSeries.java
@@ -6,39 +6,47 @@ public class GraphSeries {
     private final float[] buffer;
     private int head = 0;
     private final int bufferSize;
-
+    
     private float maxScale = 60f;
     private final float minScale = 1f;
     private final Color color;
-
+    
     public GraphSeries(int bufferSize, Color color) {
         this.bufferSize = bufferSize;
         this.buffer = new float[bufferSize];
         this.color = color;
     }
-
+    
     public void update(float value) {
         buffer[head] = value;
         head = (head + 1) % bufferSize;
-
+        
         // Autoscaling logic
         float currentBufferMax = 0;
         for (float val : buffer) {
             if (val > currentBufferMax) currentBufferMax = val;
         }
-
+        
         if (currentBufferMax > maxScale) {
             maxScale *= 2;
         } else if (currentBufferMax < maxScale / 2 && maxScale > minScale) {
             maxScale /= 2;
         }
     }
-
+    
     public float getValueAt(int chronologicalIndex) {
         return buffer[(head + chronologicalIndex) % bufferSize];
     }
-
-    public int getBufferSize() { return bufferSize; }
-    public float getMaxScale() { return maxScale; }
-    public Color getColor() { return color; }
+    
+    public int getBufferSize() {
+        return bufferSize;
+    }
+    
+    public float getMaxScale() {
+        return maxScale;
+    }
+    
+    public Color getColor() {
+        return color;
+    }
 }
