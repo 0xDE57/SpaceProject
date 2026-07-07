670695625f4f50fa48da7b9560e1050d564de135
arbitrary hexcode <0XDE57@users.noreply.github.com>
2026-07-07 02:15:36 -0600

    remove uneeded comments

--- core/src/main/java/com/spaceproject/utility/MultiGraph.java
+++ core/src/main/java/com/spaceproject/utility/MultiGraph.java
@@ -30,11 +30,9 @@ public class MultiGraph {
     }
     
     public void draw(ShapeRenderer shape, float x, float y) {
-        // 1. Draw Background
         shape.setColor(new Color(0.1f, 0.1f, 0.1f, 1f));
         shape.rect(x, y, width, height);
         
-        // 2. Draw each series
         for (GraphSeries series : seriesList) {
             shape.setColor(series.getColor());
             
@@ -46,10 +44,7 @@ public class MultiGraph {
             for (int i = 0; i < size; i++) {
                 float val = series.getValueAt(i);
                 
-                // X is mapped to the series size
                 float posX = x + ((float) i / size) * width;
-                
-                // Y is mapped to this specific series' scale
                 float posY = y + (Math.max(0, val) / scale) * height;
                 
                 if (prevX != -1) {
