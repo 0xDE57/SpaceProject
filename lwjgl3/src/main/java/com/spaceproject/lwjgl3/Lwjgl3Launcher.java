1cb1b01cad4aa84b154fef134727a28dc8980e3b
arbitrary hexcode <0XDE57@users.noreply.github.com>
2026-08-09 17:48:19 -0600

    up lwjgl to 3.4.2, and set platform_x11 hint
    workaround fixes fullscreen issue in 23112de

--- lwjgl3/src/main/java/com/spaceproject/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/spaceproject/lwjgl3/Lwjgl3Launcher.java
@@ -3,6 +3,8 @@ package com.spaceproject.lwjgl3;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.spaceproject.SpaceProject;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.system.Platform;
 
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
@@ -11,6 +13,9 @@ public class Lwjgl3Launcher {
     }
 
     private static Lwjgl3Application createApplication(String[] args) {
+        if (Platform.get() == Platform.LINUX) {
+            GLFW.glfwInitHint(GLFW.GLFW_PLATFORM, GLFW.GLFW_PLATFORM_X11);
+        }
         return new Lwjgl3Application(new SpaceProject(args), getDefaultConfiguration());
     }
 
